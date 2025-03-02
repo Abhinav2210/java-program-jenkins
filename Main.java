f517861712dbf0979278762d55f2b75dd9ebd262
Abhinav Yadav <Abhinav2210@github.com>
2025-03-02 10:38:28 +0530

    Updated Main.java
    added one more line

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello this is java program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("working on jenkins");
     }
 }
